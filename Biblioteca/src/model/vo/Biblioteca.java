e1af7c3287ffbf008c71330fbc190b2a0762404e
Guilherme-Valle <guidovalle1608@gmail.com>
2018-06-20 15:35:05 -0300

    Biblioteca com classes

--- Biblioteca/src/model/vo/Biblioteca.java
+++ Biblioteca/src/model/vo/Biblioteca.java
@@ -5,10 +5,70 @@
  */
 package model.vo;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  *
  * @author guido
  */
 public class Biblioteca {
+    
+    private HashSet<Usuario> usuarios;
+    private HashSet<Livro> livros;
+    private ArrayList<Emprestimo> emprestimos;
+    private String nome;
+    private Administrador adm;
+
+    public Biblioteca(String nome, Administrador adm) {
+        this.nome = nome;
+        this.adm = adm;
+        this.usuarios = new HashSet<Usuario>();
+        this.livros = new HashSet<Livro>();
+        this.emprestimos = new ArrayList<Emprestimo>();
+    }
+
+    public HashSet<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(HashSet<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public HashSet<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(HashSet<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public ArrayList<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(ArrayList<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Administrador getAdm() {
+        return adm;
+    }
+
+    public void setAdm(Administrador adm) {
+        this.adm = adm;
+    }
+    
+    
+    
     
 }
